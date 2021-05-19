e4d4f5dcfe260db7db7aabf4e8ddfc1fa92c5650
carlvskansas <48308402+carlgwsmith@users.noreply.github.com>
2021-05-18 22:19:21 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -7,20 +7,29 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
+  const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    fetch("https://coinranking1.p.rapidapi.com/coins", {
+fetch("https://coinranking1.p.rapidapi.com/coins", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "576a270f4emshce03cc0d892e394p15648fjsnddb66ef301e9",
 		"x-rapidapi-host": "coinranking1.p.rapidapi.com"
 	}
+})
+.then(response => {
+  if(response.ok){
+    response.json().then((json) => {
+      //console.log(json.data.coins)
+      setCoins(json.data.coins)
+      setIsLoading(false);
     })
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.error(err);
-    });
+  }
+})
+.catch(err => {
+	console.error(err);
+});
 }, []);
 
   return (
@@ -36,6 +45,21 @@ function Home() {
         <Banner />
         <div className="row">
           <div className="col-sm-5"><Search label="Let's analyze a crypto"/></div>
+          <div className="col-sm-5">
+          {isLoading && <p>Wait I'm Loading comments for you</p>}
+            {coins.length !== 6}
+            {coins.slice(0,6).map((c, index) => (
+              <div key={index}>
+                    <div>
+                      <h2 style={{ textDecoration: "Underline" }}>
+                        {c.name}
+                      </h2>
+                      <p>{c.volume}</p>
+                    </div>
+                    <hr />
+              </div>
+            ))}
+          </div>
         </div>
     </div>
   );
