97989353914fa0238ce2d37c50d8d72ef761d1a2
carlvskansas <48308402+carlgwsmith@users.noreply.github.com>
2021-05-25 23:18:03 -0500

    navbar with dropdown

--- src/Home.js
+++ src/Home.js
@@ -15,10 +15,10 @@ function Home() {
         <TopSlider/>
       <div className="row topCryptos">
       </div>
-        <Banner />
         <div className="row">
-          <div className="col-sm-5"><Search label="Let's analyze a crypto"/></div>
-          <div className="col-sm-5">
+          <div className="col-sm-6"><Banner /></div>
+          <div className="col-sm-6"><Search label="Let's analyze a crypto"/></div>
+          <div className="col-sm-12">
             <TinyChart/>
           </div>
         </div>
--- src/components/CustomNav.js
+++ src/components/CustomNav.js
@@ -1,4 +1,8 @@
 import { Nav, Navbar } from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
+import {FiLogOut} from 'react-icons/fi'
+import {CgProfile} from 'react-icons/cg'
+import {FaCog} from 'react-icons/fa'
 import '../components/CSS/Nav.css'
 
 function CustomNav(){
@@ -14,6 +18,17 @@ function CustomNav(){
           <Nav.Link href="/Results">
             Results
           </Nav.Link>
+          <Dropdown alignRight>
+            <Dropdown.Toggle id="dropdown-basic" as="p">
+               <img src={`${process.env.PUBLIC_URL}/Assets/carl.jpg`} className="profilepic"></img>
+            </Dropdown.Toggle>
+            <Dropdown.Menu >
+              <Dropdown.Item href="#">Profile <CgProfile className="ddicon"/></Dropdown.Item>
+              <Dropdown.Item href="#">Settings <FaCog className="ddicon"/></Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item href="#">Log Out <FiLogOut className="ddicon"/></Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
