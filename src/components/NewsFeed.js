8913753e0378067927021bb165cbb19bee56f694
carlvskansas <48308402+carlgwsmith@users.noreply.github.com>
2021-07-14 22:33:21 -0500

    styling fixes

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -8,15 +8,26 @@ const NewsFeed = (props) => {
       setNews(props.news)
     }, [props]);
 
+    function truncateString(string, limit) {
+      if (string.length > limit) {
+        return string.substring(0, limit) + "..."
+      } else {
+        return string
+      }
+    }
+
     return (
         <>
             {news.slice(0,6).map((article, index) => (
               <div key={index} className="col-sm-4">
+                <div className="news">
                 <a href={article.link}>
-               <p className="newsHeadline">{article.title}</p>
-               <p className="via">{article.clean_url}</p>
-               <p className="newsSummary">{article.summary}</p>
-               </a>
+                  <img style={{backgroundImage: "url('"+article.media +"')"}} className="articleImage"></img>
+                  <p className="newsHeadline">{truncateString(article.title, 40)}</p>
+                  <p className="via">{article.author} | {article.clean_url}</p>
+                  <p className="newsSummary">{truncateString(article.summary, 200)}</p>
+                </a>
+               </div>
               </div>
             ))}
         </>
